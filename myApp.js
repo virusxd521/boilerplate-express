86aa4488ade7bb7bbffc4f71dc1353e65ea61e12
Daniel <virusxd521@gmail.com>
2022-04-04 11:23:42 +0300

    JSON route has been added

--- myApp.js
+++ myApp.js
@@ -11,9 +11,13 @@ app.all('/', (req, res) => {
 // The public route, with a middleware
 app.use('/public', express.static(`${__dirname}/public`));
 
-
-
-
+// Response with a JSON object
+app.get('/json', (req, res) => {
+    const obj =  {
+        message: "Hello json"
+    };
+    res.json(obj);
+});
 
 
 
