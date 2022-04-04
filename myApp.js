27d3493fc22da371305b9bfb0b62c868058c994d
Daniel <virusxd521@gmail.com>
2022-04-04 10:54:40 +0300

    added a post method cause of the strict origin issue

--- myApp.js
+++ myApp.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 
 
+app.post('/', (req, res) => {
+    res.sendFile(`${__dirname}/views/index.html`);
+});
 
 app.all('/', (req, res) => {
     res.sendFile(`${__dirname}/views/index.html`);
