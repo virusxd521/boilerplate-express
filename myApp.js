2602c069ad62d98e27f176296e6104f2735c0eb8
Daniel <virusxd521@gmail.com>
2022-04-04 11:08:51 +0300

    adding static assets to my project

--- myApp.js
+++ myApp.js
@@ -2,9 +2,14 @@ var express = require('express');
 var app = express();
 
 
+
+// The home route
 app.all('/', (req, res) => {
     res.sendFile(`${__dirname}/views/index.html`);
 });
+
+// The public route, with a middleware
+app.use('/public', express.static(`${__dirname}/public`));
 
 
 
